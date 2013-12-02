3c8aac90014d7f6f4a292ad1612a76959ae8d30d
Gady Schujovitzky <gaaady@gmail.com>
2013-12-02 11:44:27 +0200

    add cart step

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -1,18 +1,7 @@
 Spree::Order.class_eval do
-  attr_accessor  :tafnit_id
-  checkout_flow do
-    go_to_state :cart
-    go_to_state :address
-    go_to_state :delivery
-    go_to_state :payment, if: ->(order) {
-      order.update_totals
-      order.payment_required?
-    }
-    go_to_state :confirm, if: ->(order) { order.confirmation_required? }
-    go_to_state :complete
-    add_transition :from => :cart, :to => :address
-    remove_transition :from => :delivery, :to => :confirm
-  end
+  #attr_accessor  :tafnit_id
+  insert_checkout_step :cart, :before => :address
+
   state_machine do
     after_transition to: :complete do |order|
     end
